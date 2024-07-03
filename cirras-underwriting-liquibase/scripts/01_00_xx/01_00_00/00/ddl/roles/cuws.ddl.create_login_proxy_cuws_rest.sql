da16f9b00394723c25d478f5f46e727bdbf7b887
Chris Preston <cpreston@vividsolutions.com>
2024-07-03 16:03:51 -0700

    Add 3.3.0 DB scripts

--- cirras-underwriting-liquibase/scripts/01_00_xx/01_00_00/00/ddl/roles/cuws.ddl.create_login_proxy_cuws_rest.sql
+++ cirras-underwriting-liquibase/scripts/01_00_xx/01_00_00/00/ddl/roles/cuws.ddl.create_login_proxy_cuws_rest.sql
@@ -15,3 +15,5 @@ ALTER ROLE proxy_cuws_rest SET search_path TO cuws;
 ALTER USER proxy_cuws_rest set TIMEZONE to 'America/New_York';
 
 COMMENT ON ROLE "proxy_cuws_rest" IS 'Proxy account for Cirras Underwriting System.';
+
+GRANT USAGE ON SCHEMA "cuws" TO "app_cuws_rest_proxy";
---
+++ cirras-underwriting-liquibase/scripts/03_00_xx/03_03_00/00/ddl/grants/cuws.ddl.apply_grants.sql
@@ -0,0 +1,2 @@
+--Grant read only access to all tables for the readonly role
+GRANT SELECT ON ALL TABLES IN SCHEMA cuws TO "app_cuws_readonly";
