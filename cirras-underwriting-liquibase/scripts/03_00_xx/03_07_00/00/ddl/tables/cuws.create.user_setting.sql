e70658979b187f543af2f1eb89b54b8bcde72e28
vivid-esnauffer <esnauffer@vividsolutions.com>
2025-01-03 16:03:24 -0800

    PIM-1847: Create User Settings View - Database - updates from tech review

--- cirras-underwriting-liquibase/scripts/03_00_xx/03_07_00/00/ddl/tables/cuws.create.user_setting.sql
+++ cirras-underwriting-liquibase/scripts/03_00_xx/03_07_00/00/ddl/tables/cuws.create.user_setting.sql
@@ -1,18 +1,18 @@
 \qecho Create user_setting table
 CREATE TABLE cuws.user_setting(
-    underwriting_user_guid    varchar(32)      NOT NULL,
-    login_user_guid           varchar(32)      NOT NULL,
-    login_user_id             varchar(64)      NOT NULL,
-    login_user_type           varchar(64)      NOT NULL,
-    given_name                varchar(50),
-    family_name               varchar(50),
-    crop_year                 numeric(4, 0)    NOT NULL,
-    insurance_plan_id         numeric(9, 0)    NOT NULL,
-    office_id                 numeric(9, 0)    NOT NULL,
-    create_user               varchar(64)      NOT NULL,
-    create_date               timestamp(0)     NOT NULL,
-    update_user               varchar(64)      NOT NULL,
-    update_date               timestamp(0)     NOT NULL
+    underwriting_user_guid             varchar(32)      NOT NULL,
+    login_user_guid                    varchar(32)      NOT NULL,
+    login_user_id                      varchar(64)      NOT NULL,
+    login_user_type                    varchar(64)      NOT NULL,
+    given_name                         varchar(50),
+    family_name                        varchar(50),
+    policy_search_crop_year            numeric(4, 0)    NOT NULL,
+    policy_search_insurance_plan_id    numeric(9, 0)    NOT NULL,
+    policy_search_office_id            numeric(9, 0)    NOT NULL,
+    create_user                        varchar(64)      NOT NULL,
+    create_date                        timestamp(0)     NOT NULL,
+    update_user                        varchar(64)      NOT NULL,
+    update_date                        timestamp(0)     NOT NULL
 ) TABLESPACE pg_default
 ;
 
@@ -28,11 +28,11 @@ COMMENT ON COLUMN cuws.user_setting.given_name IS 'Given Name is the given name 
 ;
 COMMENT ON COLUMN cuws.user_setting.family_name IS 'Family Name is the family name from the authentication for a user '
 ;
-COMMENT ON COLUMN cuws.user_setting.crop_year IS 'Crop Year is the user''s preferred crop year to search for on the policy screen'
+COMMENT ON COLUMN cuws.user_setting.policy_search_crop_year IS 'Policy Search Crop Year is the user''s preferred crop year to search for on the policy screen'
 ;
-COMMENT ON COLUMN cuws.user_setting.insurance_plan_id IS 'Insurance Plan Id is the user''s  preferred insurance plan to search for on the policy screen.'
+COMMENT ON COLUMN cuws.user_setting.policy_search_insurance_plan_id IS 'Policy Search Insurance Plan Id is the user''s  preferred insurance plan to search for on the policy screen.'
 ;
-COMMENT ON COLUMN cuws.user_setting.office_id IS 'Office Id is the user''s prefered office to search for on the policy screen.'
+COMMENT ON COLUMN cuws.user_setting.policy_search_office_id IS 'Policy Search Office Id is the user''s preferred office to search for on the policy screen.'
 ;
 COMMENT ON COLUMN cuws.user_setting.create_user IS 'Create User is the user id of the user that created the record '
 ;
@@ -48,6 +48,12 @@ COMMENT ON TABLE cuws.user_setting IS 'The table contains user''s preferred sett
 CREATE INDEX ix_us_lug ON cuws.user_setting(login_user_guid)
  TABLESPACE pg_default
 ;
+CREATE INDEX ix_us_psip ON cuws.user_setting(policy_search_insurance_plan_id)
+ TABLESPACE pg_default
+;
+CREATE INDEX ix_us_psof ON cuws.user_setting(policy_search_office_id)
+ TABLESPACE pg_default
+;
 ALTER TABLE cuws.user_setting ADD 
     CONSTRAINT pk_us PRIMARY KEY (underwriting_user_guid) USING INDEX TABLESPACE pg_default 
 ;
@@ -57,11 +63,11 @@ ALTER TABLE cuws.user_setting ADD
 ;
 
 ALTER TABLE cuws.user_setting ADD CONSTRAINT fk_us_ip 
-    FOREIGN KEY (insurance_plan_id)
+    FOREIGN KEY (policy_search_insurance_plan_id)
     REFERENCES cuws.insurance_plan(insurance_plan_id)
 ;
 
 ALTER TABLE cuws.user_setting ADD CONSTRAINT fk_us_of 
-    FOREIGN KEY (office_id)
-    REFERENCES cuws.office(office_id) ON DELETE RESTRICT ON UPDATE RESTRICT
+    FOREIGN KEY (policy_search_office_id)
+    REFERENCES cuws.office(office_id)
 ;
