a3da264b2cd5e7a90aaeac2a10e1e90f721f8c65
vivid-esnauffer <esnauffer@vividsolutions.com>
2025-01-28 17:11:09 -0800

    Removed debugging

--- cirras-underwriting-war/cirras-underwriting-war/src/main/angular/src/app/components/verified-yield/commodity-list/verified-yield-commodity-list.component.ts
+++ cirras-underwriting-war/cirras-underwriting-war/src/main/angular/src/app/components/verified-yield/commodity-list/verified-yield-commodity-list.component.ts
@@ -20,7 +20,6 @@ export class VerifiedYieldCommodityListComponent  {
   goToYieldCalculatorLink(){
 
     let yieldCalculatorLink = this.appConfigService.getConfig().rest["grain_verified_yield_calculator_url"]
-    console.log("yieldCalculatorLink : " + yieldCalculatorLink)
     window.open(yieldCalculatorLink, "_blank");
   }
 
