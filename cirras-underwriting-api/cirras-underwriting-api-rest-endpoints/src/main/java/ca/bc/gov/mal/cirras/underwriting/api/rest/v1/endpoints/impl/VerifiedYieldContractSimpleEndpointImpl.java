2abf905bd9781c93a6c11ad12df48f0e5e7b4bd1
Michael Hostettler <michael.hostettler@gov.bc.ca>
2025-05-22 10:25:45 -0700

    PIM-2016: Removed unused code

--- cirras-underwriting-api/cirras-underwriting-api-rest-endpoints/src/main/java/ca/bc/gov/mal/cirras/underwriting/api/rest/v1/endpoints/impl/VerifiedYieldContractSimpleEndpointImpl.java
+++ cirras-underwriting-api/cirras-underwriting-api-rest-endpoints/src/main/java/ca/bc/gov/mal/cirras/underwriting/api/rest/v1/endpoints/impl/VerifiedYieldContractSimpleEndpointImpl.java
@@ -1,21 +1,14 @@
 package ca.bc.gov.mal.cirras.underwriting.api.rest.v1.endpoints.impl;
 
-import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import ca.bc.gov.nrs.common.wfone.rest.resource.MessageListRsrc;
 import ca.bc.gov.nrs.wfone.common.rest.endpoints.BaseEndpointsImpl;
-import ca.bc.gov.nrs.wfone.common.service.api.ConflictException;
-import ca.bc.gov.nrs.wfone.common.service.api.ForbiddenException;
 import ca.bc.gov.nrs.wfone.common.service.api.NotFoundException;
-import ca.bc.gov.nrs.wfone.common.service.api.ValidationFailureException;
 import ca.bc.gov.mal.cirras.underwriting.service.api.v1.CirrasVerifiedYieldService;
 import ca.bc.gov.mal.cirras.underwriting.api.rest.v1.resource.VerifiedYieldContractSimpleRsrc;
-import ca.bc.gov.mal.cirras.underwriting.api.rest.v1.endpoints.VerifiedYieldContractEndpoint;
 import ca.bc.gov.mal.cirras.underwriting.api.rest.v1.endpoints.VerifiedYieldContractSimpleEndpoint;
 import ca.bc.gov.mal.cirras.underwriting.api.rest.v1.endpoints.security.Scopes;
 
@@ -23,37 +16,6 @@ public class VerifiedYieldContractSimpleEndpointImpl extends BaseEndpointsImpl i
 		
 	@Autowired
 	private CirrasVerifiedYieldService cirrasVerifiedYieldService;
-	
-	
-//	@Override
-//	public Response getVerifiedYieldContract(final String verifiedYieldContractGuid) {
-//		
-//		Response response = null;
-//		
-//		logRequest();
-//		
-//		if(!hasAuthority(Scopes.GET_VERIFIED_YIELD_CONTRACT)) {
-//			return Response.status(Status.FORBIDDEN).build();
-//		}
-//		
-//		try {
-//			VerifiedYieldContractRsrc result = (VerifiedYieldContractRsrc) cirrasVerifiedYieldService.getVerifiedYieldContract(
-//					verifiedYieldContractGuid,
-//					getFactoryContext(), 
-//					getWebAdeAuthentication());
-//			response = Response.ok(result).tag(result.getUnquotedETag()).build();
-//
-//		} catch (NotFoundException e) {
-//			response = Response.status(Status.NOT_FOUND).build();
-//			
-//		} catch (Throwable t) {
-//			response = getInternalServerErrorResponse(t);
-//		}
-//		
-//		logResponse(response);
-//
-//		return response;
-//	}
 
 	@Override
 	public Response getVerifiedYieldContractSimple(
