641215de628be705090303977b593e467d1a08d2
Vividsolutions/esnauffer <esnauffer@vividsolutions.com>
2025-10-01 13:13:05 -0700

    PIM-2204: Blueberry Inventory - UI - Create Screen

--- cirras-underwriting-war/cirras-underwriting-war/src/main/angular/src/app/components/inventory/berries/berries-inventory.component.ts
+++ cirras-underwriting-war/cirras-underwriting-war/src/main/angular/src/app/components/inventory/berries/berries-inventory.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, ViewEncapsulation } from '@angular/core';
+import { BaseComponent } from '../../common/base/base.component';
+import { CropCommodityList, InventoryContract, UwContract } from 'src/app/conversion/models';
+import { BerriesInventoryComponentModel } from './berries-inventory.component.model';
 
 @Component({
   selector: 'berries-inventory',
@@ -8,6 +11,29 @@ import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/
   encapsulation: ViewEncapsulation.None,
   standalone: false
 })
-export class BerriesInventoryComponent { // TODO: extends BaseComponent implements OnChanges 
+export class BerriesInventoryComponent extends BaseComponent implements OnChanges {
+  @Input() inventoryContract: InventoryContract;
+  @Input() cropCommodityList: CropCommodityList;
+  @Input() growerContract: UwContract;
 
+  hasDataChanged = false;
+
+  initModels() {
+    this.viewModel = new BerriesInventoryComponentModel(this.sanitizer, this.fb, this.inventoryContract);
+  }
+
+  loadPage() {
+      // TODO
+  }
+
+  getViewModel(): BerriesInventoryComponentModel  { //
+      return <BerriesInventoryComponentModel>this.viewModel;
+  }
+
+  setFormSeededStyles(){
+    return {
+      'grid-template-columns':  'auto 140px 150px 12px 190px'
+    }
+  }
+  
 }
