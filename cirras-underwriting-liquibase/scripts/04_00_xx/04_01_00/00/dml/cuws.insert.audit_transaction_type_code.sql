b0394e6ac899ac8a86bc3be67ada529f94ff5a6d
Vividsolutions/esnauffer <esnauffer@vividsolutions.com>
2026-03-31 16:06:28 -0700

    PIM-2529: DML script

---
+++ cirras-underwriting-liquibase/scripts/04_00_xx/04_01_00/00/dml/cuws.insert.audit_transaction_type_code.sql
@@ -0,0 +1,11 @@
+INSERT INTO cuws.audit_transaction_type_code(
+	audit_transaction_type_code, description)
+	VALUES ('INSERT', 'Insert');
+
+INSERT INTO cuws.audit_transaction_type_code(
+	audit_transaction_type_code, description)
+	VALUES ('UPDATE', 'Update');
+
+INSERT INTO cuws.audit_transaction_type_code(
+	audit_transaction_type_code, description)
+	VALUES ('DELETE', 'Delete');
